77c703f8f93b42a8a35d686a19a0aa7899b41e1c
Andrea Wu <angeeya.wu@gmail.com>
2022-12-21 00:31:14 -0600

    product css

--- react-app/src/components/Products/ProductBrowser.js
+++ react-app/src/components/Products/ProductBrowser.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { thunkLoadAllProducts } from "../../store/product"
+import { Link } from "react-router-dom"
 import noimage from "../Images/noimage.jpg"
+import "./Products.css"
 
 const ProductBrowser = () => {
   const dispatch = useDispatch()
@@ -17,7 +19,7 @@ const ProductBrowser = () => {
     <>
       {productsArr.map((product) => (
         <div className='product-card' key={product.id}>
-          {/* <Link to={`/products/${product.id}`}> */}
+          <Link to={`/products/${product.id}`}>
             <div className='product-image'>
                 <img
                   src={product.Images[0].url}
@@ -25,11 +27,11 @@ const ProductBrowser = () => {
                   onError={e => e.target.src = {noimage}}
                 />
             </div>
-          {/* </Link> */}
+          </Link>
           <div className='product-card-info'>
-            {/* <Link to={`/products/${product.id}`}> */}
+            <Link to={`/products/${product.id}`}>
                 <p className='product-card-name'>{product.name}</p>
-            {/* </Link> */}
+            </Link>
             <p className='product-card-price'>${product.price}</p>
             <p className='product-card-free-shipping'>Free Shipping</p>
           </div>
