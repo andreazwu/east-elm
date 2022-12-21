acdc1ed75bb1238080429926691990cda8599646
Andrea Wu <angeeya.wu@gmail.com>
2022-12-21 11:56:21 -0600

    basic nav bar

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -1,10 +1,33 @@
 import React from "react"
-import { NavLink } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { Link, NavLink } from "react-router-dom"
 import LogoutButton from "../auth/LogoutButton"
 import logo from "../Images/logo.jpg"
 import "./Navigation.css"
 
 const NavBar = () => {
+  const user = useSelector((state) => state.session.user)
+
+  let sessionLinks
+  if (user) {
+    sessionLinks = (
+      <ul>
+        <li><Link to={`/users/${user.id}`}>My Account</Link></li>
+        <li><Link to={"/my-products"}>My Products</Link></li>
+        {/* <li><Link to={"/my-orders"}>My Orders</Link></li>
+        <li><Link to={"/my-reviews"}>My Reviews</Link></li> */}
+        <li><LogoutButton /></li>
+      </ul>
+    )
+  } else {
+    sessionLinks = (
+      <ul>
+        <li><Link to={"/login"}>Log In</Link></li>
+        <li><Link to={"/signup"}>Sign Up</Link></li>
+      </ul>
+    )
+  }
+
   return (
     <nav>
       <div>
@@ -14,22 +37,29 @@ const NavBar = () => {
           </div>
         </NavLink>
       </div>
+
+      <div>
+        <div>
+          <i className="fa-regular fa-user"></i>
+          &nbsp;
+          {
+            user ?
+            "Account" :
+            "Log In/ Sign Up"
+          }
+        </div>
+        <div>
+          {sessionLinks}
+        </div>
+      </div>
+
       <ul className="navbar">
         <li>
           <NavLink to="/" exact={true} activeClassName="active">
             Home
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
+
         <li>
           <NavLink to="/users" exact={true} activeClassName="active">
             Users
@@ -39,9 +69,6 @@ const NavBar = () => {
           <NavLink to="/products" exact={true} activeClassName="active">
             Products
           </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
         </li>
       </ul>
     </nav>
