1ae4f861909404538f8087c9c96b30d001e4735e
Andrea Wu <angeeya.wu@gmail.com>
2023-01-03 14:57:11 -0600

    delete review

--- react-app/src/components/Reviews/UserReviews/DeleteReviewAlert.js
+++ react-app/src/components/Reviews/UserReviews/DeleteReviewAlert.js
@@ -1,8 +1,30 @@
 import React from "react"
+import { useDispatch } from "react-redux"
+import { thunkRemoveReview } from "../../../store/review"
 
-const DeleteReviewAlert = () => {
+const DeleteReviewAlert = ({id, setShowDeleteReviewAlert}) => {
+  const dispatch = useDispatch()
+
+  const handleDelete = async (e) => {
+    e.preventDefault()
+    await dispatch(thunkRemoveReview(id))
+      .then(() => setShowDeleteReviewAlert(false))
+  }
   return (
-    <div>DeleteReviewAlert</div>
+    <div>
+      <h4>Delete Review</h4>
+      <div>
+        <p>Are you sure you wish to delete this review? This cannot be undone.</p>
+        <div>
+          <button onClick={()=>setShowDeleteReviewAlert(false)}>
+            CANCEL
+          </button>
+          <button onClick={handleDelete}>
+            DELETE
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
