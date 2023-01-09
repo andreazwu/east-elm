74470ffd8cc0b6de7bc83e507bcd82e0f9b2f78a
Andrea Wu <angeeya.wu@gmail.com>
2023-01-09 08:11:44 -0600

    product details css

--- react-app/src/components/Products/UserProducts/MyProduct.js
+++ react-app/src/components/Products/UserProducts/MyProduct.js
@@ -1,23 +1,22 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import { Modal } from "../../../context/Modal"
-import DeleteProductAlert from "./DeleteProductAlert"
-import EditProductForm from "./EditProductForm"
-import noimage from "../../Images/noimage.jpg"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Modal } from "../../../context/Modal";
+import DeleteProductAlert from "./DeleteProductAlert";
+import EditProductForm from "./EditProductForm";
+import noimage from "../../Images/noimage.jpg";
 
 const MyProduct = ({ product }) => {
-  const [showEditModal, setShowEditModal] = useState(false)
-  const [showDeleteAlert, setShowDeleteAlert] = useState(false)
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
 
   return (
     <div className="myproduct-wrapper">
-
       <div className="myproduct-img">
         <Link to={`/products/${product.id}`}>
           <img
             src={product.Images[0].url}
             alt="myproduct-img"
-            onError={e => e.target.src={noimage}}
+            onError={(e) => (e.target.src = noimage)}
           />
         </Link>
       </div>
@@ -27,40 +26,47 @@ const MyProduct = ({ product }) => {
           <div className="myproduct-name">{product.name}</div>
         </Link>
         <div className="myproduct-category">{product.category}</div>
-        <div className="myproduct-price">${Number(product.price).toFixed(2)}</div>
+        <div className="myproduct-price">
+          ${Number(product.price).toFixed(2)}
+        </div>
       </div>
 
       <div className="myproduct-buttons-container">
-        <button className="myproduct-edit" onClick={()=>setShowEditModal(true)}>
+        <button
+          className="myproduct-edit"
+          onClick={() => setShowEditModal(true)}
+        >
           EDIT
         </button>
         <div>
-        {showEditModal && (
-          <Modal onClose={()=>setShowEditModal(false)}>
-            <EditProductForm
-              setShowEditModal={setShowEditModal}
-              product={product}
-            />
-          </Modal>
-        )}
+          {showEditModal && (
+            <Modal onClose={() => setShowEditModal(false)}>
+              <EditProductForm
+                setShowEditModal={setShowEditModal}
+                product={product}
+              />
+            </Modal>
+          )}
         </div>
-        <button className="myproduct-delete" onClick={()=>setShowDeleteAlert(true)}>
+        <button
+          className="myproduct-delete"
+          onClick={() => setShowDeleteAlert(true)}
+        >
           DELETE
         </button>
         <div>
-        {showDeleteAlert && (
-          <Modal onClose={()=>setShowDeleteAlert(false)}>
-            <DeleteProductAlert
-              setShowDeleteAlert={setShowDeleteAlert}
-              id={product.id}
-            />
-          </Modal>
-        )}
+          {showDeleteAlert && (
+            <Modal onClose={() => setShowDeleteAlert(false)}>
+              <DeleteProductAlert
+                setShowDeleteAlert={setShowDeleteAlert}
+                id={product.id}
+              />
+            </Modal>
+          )}
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default MyProduct
+export default MyProduct;
